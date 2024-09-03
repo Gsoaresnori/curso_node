388f61a9a35ac073ff6b690513a0dccd8a4dd832
Gsoaresnori <gabrielzinho353@gmail.com>
2024-09-03 10:52:05 -0300

    inserindo tipo em controllers

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { createUser, getUsers } from './user.service';
+import { UserInsertDTO } from './dtos/user-insert.dto';
 
 const userRouter = Router();
 
@@ -7,12 +8,12 @@ const router = Router();
 
 userRouter.use('/user', router);
 
-router.get('/', async (req, res) => {
+router.get('/', async (_, res: Response): Promise<void> => {
   const users = await getUsers();
   res.send(users);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async (req: Request<undefined, undefined, UserInsertDTO>, res: Response): Promise<void> => {
   const user = await createUser(req.body);
   res.send('Deu Certo');
 });
