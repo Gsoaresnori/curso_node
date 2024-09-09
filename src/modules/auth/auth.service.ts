a1a04521bd5d1573bc3e8b48b4b89f36954d561d
Gsoaresnori <gabrielzinho353@gmail.com>
2024-09-09 12:28:55 -0300

    create token JWT

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,8 +3,10 @@ import { AuthDTO } from './dtos/auth.dto';
 import { getUserByEmail } from '@modules/user/user.service';
 import { validatePassword } from '@utils/password';
 import { NotFoundException } from '@exceptions/not-found-exceptions';
+import { AuthModel } from './dtos/auth.model';
+import { generateToken } from '@utils/auth';
 
-export const validateAuth = async (authDto: AuthDTO): Promise<UserModel> => {
+export const validateAuth = async (authDto: AuthDTO): Promise<AuthModel> => {
   const user = await getUserByEmail(authDto.email);
 
   const isValidPassword = await validatePassword(authDto.password, user.password);
@@ -13,5 +15,5 @@ export const validateAuth = async (authDto: AuthDTO): Promise<UserModel> => {
     throw new NotFoundException('User');
   }
 
-  return user;
+  return new AuthModel(generateToken(user), user);
 };
---
+++ src/utils/auth.ts
@@ -0,0 +1,16 @@
+import { UserModel } from "@modules/user/user.model";
+import { sign } from 'jsonwebtoken';
+
+export const PASSWORD_JWT = 'umasenhamuitograndedepoisdemudar';
+
+export const generateToken = (user: UserModel): string => {
+
+    return sign({
+        userId: user.id,
+        email: user.email,
+        typeUser: user.typeUser,
+    }, PASSWORD_JWT, {
+        subject: String(user.id),
+        expiresIn: '604800000',
+    })
+}
